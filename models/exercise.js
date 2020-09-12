4c1dfd2f48edd92c551e888ec36f0b1175d4e08f
Anthony Bergen <anthonydbergen@gmail.com>
2020-09-12 11:51:22 -0400

    finished exercise model

--- models/exercise.js
+++ models/exercise.js
@@ -1,1 +1,17 @@
 const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+  type: String,
+  name: String,
+  distance: Number,
+  duration: Number,
+  weight: Number,
+  sets: Number,
+  reps: Number,
+});
+
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+module.exports = Exercise;
