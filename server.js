bb7b9c6aa8fb3aa902b2c6e44e17f43b76bdeb60
Anthony Bergen <anthonydbergen@gmail.com>
2020-09-16 10:44:12 -0400

    changing method to router

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
