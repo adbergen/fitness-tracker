dc4623e3f30ca82aee439791fe80855334c7371f
Anthony Bergen <anthonydbergen@gmail.com>
2020-09-16 11:39:32 -0400

    reworked workout.js to include types

--- models/workout.js
+++ models/workout.js
@@ -2,22 +2,58 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now(),
-  },
-  exercises: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise",
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date(),
     },
-  ],
-
-  totalDuration: {
-    type: Number,
-    default: 0,
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Select Resistance or Cardio",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Exercise name required.",
+        },
+        duration: {
+          type: Number,
+          required: "Enter exercise duration time.",
+        },
+        weight: {
+          type: Number,
+          required: "Weight required.",
+        },
+        reps: {
+          type: Number,
+          required: "Number of reps required.",
+        },
+        sets: {
+          type: Number,
+          required: "Number of sets required.",
+        },
+        distance: {
+          type: Number,
+          required: "Distance required.",
+        },
+      },
+    ],
   },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
